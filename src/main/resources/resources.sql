2e53910c304444c680f379ce1b56c672a812f5ca
Ana Ferreira <anadedavid@gmail.com>
2019-01-26 17:27:49 -0200

    resources

---
+++ src/main/resources/resources.sql
@@ -0,0 +1,8 @@
+insert into livro (id, nome, autor, descricao, editora)
+values (1, 'livro a','autor a','descricao a', 'editora a');
+
+insert into livro (id, nome, autor, descricao, editora)
+values (2, 'livro b','autor b','descricao b', 'editora b');
+
+insert into livro (id, nome, autor, descricao, editora)
+values (3, 'livro c','autor c','descricao c', 'editora c');
